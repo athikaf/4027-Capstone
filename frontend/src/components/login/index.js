82c36c6fac6ab880be301fbc6567cc4d9d14633a
Vikram <singh.vikramsingh06@gmail.com>
2024-04-17 12:53:39 -0400

    mobilw view added

--- frontend/src/components/login/index.js
+++ frontend/src/components/login/index.js
@@ -31,92 +31,6 @@ const Login = ({ isLoggedIn, handleLogin }) => {
     dispatch(handleSignIn(payload));
   };
   return (
-    //     <div className='App'>
-    //       <div
-    //   data-id="LgOaV3zR"
-    //   className="V69BwE perFpF"
-    //   data-background-prevent="true"
-    //   style={{
-    //     top: "0px",
-    //     left: "0px",
-    //     width: "190px",
-    //     height: "166.247px",
-    //     transform: "rotate(0deg)"
-    //   }}
-    // ></div>
-
-    //       <div className='login-container'>
-    //         <h1>Login</h1>
-    //         <form onSubmit={handleSubmit}>
-    //           <div className='form-group'>
-    //             <label>Email</label>
-    //             <input
-    //               type='email'
-    //               value={email}
-    //               onChange={(e) => setEmail(e.target.value)}
-    //             />
-    //           </div>
-    //           <div className='form-group'>
-    //             <label>Password</label>
-    //             <input
-    //               type='password'
-    //               value={password}
-    //               onChange={(e) => setPassword(e.target.value)}
-    //             />
-    //           </div>
-    //           {error && <div className='error'>{error}</div>}
-    //           <button type='submit' >
-    //             Login
-    //           </button>
-    //         </form>
-    //       </div>
-    //     </div>
-    // <div className={styles.='container'>
-    //   <div className={styles.='screen'>
-    //     <div className={styles.='screen__content'>
-    //       <form className={styles.='login'>
-    //         <div className={styles.='login__field'>
-    //           <i className={styles.='login__icon fas fa-user'></i>
-    //           <input
-    //             type='text'
-    //             className={styles.='login__input'
-    //             placeholder='User name / Email'
-    //             value={email}
-    //             onChange={(e) => setEmail(e.target.value)}
-    //           />
-    //         </div>
-    //         <div className={styles.='login__field'>
-    //           <i className={styles.='login__icon fas fa-lock'></i>
-    //           <input
-    //             type='password'
-    //             className={styles.='login__input'
-    //             placeholder='Password'
-    //             value={password}
-    //             onChange={(e) => setPassword(e.target.value)}
-    //           />
-    //         </div>
-    //         <button className={styles.='button login__submit'>
-    //           <span className={styles.='button__text'>Log In Now</span>
-    //           <i className={styles.='button__icon fas fa-chevron-right'></i>
-    //         </button>
-    //       </form>
-    //       <div className={styles.='social-login'>
-    //         <h3>log in via</h3>
-    //         <div className={styles.='social-icons'>
-    //           <a href='#' className={styles.='social-login__icon fab fa-instagram'></a>
-    //           <a href='#' className={styles.='social-login__icon fab fa-facebook'></a>
-    //           <a href='#' className={styles.='social-login__icon fab fa-twitter'></a>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div className={styles.='screen__background'>
-    //       <span className={styles.='screen__background__shape screen__background__shape4'></span>
-    //       <span className={styles.='screen__background__shape screen__background__shape3'></span>
-    //       <span className={styles.='screen__background__shape screen__background__shape2'></span>
-    //       <span className={styles.='screen__background__shape screen__background__shape1'></span>
-    //     </div>
-    //   </div>
-    // </div>
     <section className={styles.section} >
     <div className={styles.container} id='container'>
       <div className={cx(styles.form_container, styles.sign_up_container)}>
@@ -161,7 +75,11 @@ const Login = ({ isLoggedIn, handleLogin }) => {
             Forgot your password?
           </a>
           <button className={styles.button}>Sign In</button>
+          <br/>
+          
         </form>
+        <div className={styles.siginUp}>  <button onClick={()=>console.log("working under progres")} >Sign Up</button></div>
+  
       </div>
       <div className={styles.overlay_container}>
         <div className={styles.overlay}>
@@ -170,7 +88,7 @@ const Login = ({ isLoggedIn, handleLogin }) => {
             <p>
               To keep connected with us please login with your personal info
             </p>
-            <button className={styles.ghost} id='signIn'>
+            <button className={styles.ghost} id='signIn' >
               Sign In
             </button>
           </div>
--- frontend/src/components/navbar/index.js
+++ frontend/src/components/navbar/index.js
@@ -1,32 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.scss";
 import cx from "classnames";
 import { useSelector, useDispatch } from "react-redux";
 import { handleLogout } from "../../Redux/actions";
 import { Navigate, useNavigate } from "react-router-dom";
 import fullLogo from "../../assets/full-logo.png";
+import menu from "../../assets/menu.png";
+import cross from "../../assets/x.png";
+
+import { ReactComponent as MenuIcon } from "../../assets/menu.svg";
+import { ReactComponent as Cross } from "../../assets/x.svg";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const isLoggedIn = useSelector((state) => state?.auth?.data);
   const user = useSelector((state) => state?.auth?.user);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   const logout = () => {
     dispatch(handleLogout());
   };
-  const navigate = useNavigate();
 
   return (
     <div>
       <nav className={styles.nav}>
-        <div className={styles.nav_logo} onClick={()=>navigate('/')}>
+        <div className={styles.nav_logo} onClick={() => navigate("/")}>
           <img src={fullLogo} alt='full' style={{ width: "200px" }} />
         </div>
-        <div className={styles.nav_menu} id='navMenu'>
+
+        <div
+          className={cx(styles.nav_menu, { [styles.active]: isMenuOpen })}
+          id='navMenu'
+        >
           <ul>
             <li>
               <a
-                onClick={() => navigate("./subsidiary_bank")}
+                onClick={() => navigate("/subsidiary_bank")}
                 className={styles.link}
               >
                 Dashboard
@@ -37,10 +55,18 @@ const Navbar = () => {
                 About
               </a>
             </li>
+
+            <li>
+              <a className={cx(styles.link,styles.userName)}>Dear, {user?.name}</a>
+            </li>
           </ul>
+          <button className={styles.btn_mobile} onClick={logout}>
+            Logout
+          </button>
         </div>
 
         <p className={styles.user}>Dear, {user?.name}</p>
+
         <div className={styles.nav_button}>
           {isLoggedIn ? (
             <button className={styles.btn} onClick={logout}>
@@ -51,14 +77,14 @@ const Navbar = () => {
               <button
                 className={cx(styles.btn, styles.white_btn)}
                 id='loginBtn'
-                onclick='login()'
+                onClick={() => navigate("/login")}
               >
                 Sign In
               </button>
               <button
                 className={styles.btn}
                 id='registerBtn'
-                onclick='register()'
+                onClick={() => navigate("/register")}
               >
                 Sign Up
               </button>
@@ -66,8 +92,12 @@ const Navbar = () => {
           )}
         </div>
 
-        <div className={styles.nav_menu_btn}>
-          <i class='bx bx-menu' onclick='myMenuFunction()'></i>
+        <div className={styles.nav_menu_btn} onClick={toggleMenu}>
+          {isMenuOpen ? (
+            <img src={cross} alt='full' style={{ width: "50px" }} />
+          ) : (
+            <img src={menu} alt='full' style={{ width: "50px" }} />
+          )}
         </div>
       </nav>
     </div>
